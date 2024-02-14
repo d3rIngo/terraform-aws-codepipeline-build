d204b47890b1a9a7775962f8e4b662a1f00e5fa2
d3rIngo <ingo.neubert92@gmx.de>
2024-02-14 11:21:31 +0100

    AWS-Pipe

---
+++ terraform/variable.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+variable "github_owner" {
+  description = "GitHub repository owner"
+  type        = string
+  default     = "d3ringo"
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "coolapp"
+}
+
+variable "github_branch" {
+  description = "GitHub repository branch"
+  type        = string
+  default     = "main"
+}
